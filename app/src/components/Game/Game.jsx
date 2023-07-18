44a564a006294c70ea4ea91fa2567e8cd065b1e1
arturohanyan1 <arturohanyan100@mail.ru>
2023-07-18 10:53:23 +0400

    refactored the code

--- app/src/components/Game/Game.jsx
+++ app/src/components/Game/Game.jsx
@@ -1,83 +1,93 @@
-import { message } from 'antd';
-import React, { useEffect, useMemo, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { deleteBotBoard } from '../../store/actionCreators/botBoard';
-import { deleteBotShips } from '../../store/actionCreators/botShips';
-import { deletePlayerBoard } from '../../store/actionCreators/playerBoard';
-import { deletePlayerShips } from '../../store/actionCreators/playerShips';
-import { getBot, getBotBoard, getBotShips, getLevel, getPlayer, getPlayerBoard, getPlayerShips, getSound } from '../../store/selectors';
-import GameBoard from '../GameBoard/GameBoard';
-import './Game.scss';
-import useSound from 'use-sound';
-import shotSound1 from '../../assets/sounds/game/shot1.mp3'
-import boomSound1 from '../../assets/sounds/game/boom1.mp3'
-import getShotCoords from '../../helpers/getShotCoords/gameShotCoords';
-import { ALREADY_SHOTED_MESSAGE, BOT_WIN_MESSAGE, PLAYER_WIN_MESSAGE } from '../../constants/constants';
-import setCrashedShipOnBoard from '../../helpers/setCrashedShipOnBoard';
-import PlayerInfoPanel from '../PlayerInfoPanel/PlayerInfoPanel';
-import { setPlayerlostCount, setPlayerWinCount } from '../../store/actionCreators/player';
-import { setBotlostCount, setBotWinCount } from '../../store/actionCreators/bot';
-
+import { message } from "antd";
+import React, { useEffect, useMemo, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { deleteBotBoard } from "../../store/actionCreators/botBoard";
+import { deleteBotShips } from "../../store/actionCreators/botShips";
+import { deletePlayerBoard } from "../../store/actionCreators/playerBoard";
+import { deletePlayerShips } from "../../store/actionCreators/playerShips";
+import {
+  getBot,
+  getBotBoard,
+  getBotShips,
+  getLevel,
+  getPlayer,
+  getPlayerBoard,
+  getPlayerShips,
+  getSound,
+} from "../../store/selectors";
+import GameBoard from "../GameBoard/GameBoard";
+import "./Game.scss";
+import useSound from "use-sound";
+import shotSound1 from "../../assets/sounds/game/shot1.mp3";
+import boomSound1 from "../../assets/sounds/game/boom1.mp3";
+import getShotCoords from "../../helpers/getShotCoords/gameShotCoords";
+import {
+  ALREADY_SHOTED_MESSAGE,
+  BOT_WIN_MESSAGE,
+  PLAYER_WIN_MESSAGE,
+} from "../../constants/constants";
+import setCrashedShipOnBoard from "../../helpers/setCrashedShipOnBoard";
+import PlayerInfoPanel from "../PlayerInfoPanel/PlayerInfoPanel";
+import {
+  setPlayerlostCount,
+  setPlayerWinCount,
+} from "../../store/actionCreators/player";
+import {
+  setBotlostCount,
+  setBotWinCount,
+} from "../../store/actionCreators/bot";
 
 const Game = () => {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
   const [messageApi, contextHolder] = message.useMessage();
   const [shot1] = useSound(shotSound1);
   const [boom1] = useSound(boomSound1);
 
-  const reduxPlayerBoard = useSelector(getPlayerBoard)
-  const reduxBotBoard = useSelector(getBotBoard)
-  const reduxPlayerShips = useSelector(getPlayerShips)
-  const reduxBotShips = useSelector(getBotShips)
+  const reduxPlayerBoard = useSelector(getPlayerBoard);
+  const reduxBotBoard = useSelector(getBotBoard);
+  const reduxPlayerShips = useSelector(getPlayerShips);
+  const reduxBotShips = useSelector(getBotShips);
   const sound = useSelector(getSound);
   const level = useSelector(getLevel);
   const player = useSelector(getPlayer);
   const bot = useSelector(getBot);
 
-  const [playerBoard, setPlayerBoard] = useState([])
-  const [playerShips, setPlayerShips] = useState([])
-  const [botBoard, setBotBoard] = useState([])
-  const [botShips, setBotShips] = useState([])
-  const [playerTurn, setPlayerTurn] = useState(true)
-  const [botCrashedShips, setBotCrashedShips] = useState([])
-  const [playerCrashedShips, setPlayerCrashedShips] = useState([])
+  const [playerBoard, setPlayerBoard] = useState([]);
+  const [playerShips, setPlayerShips] = useState([]);
+  const [botBoard, setBotBoard] = useState([]);
+  const [botShips, setBotShips] = useState([]);
+  const [playerTurn, setPlayerTurn] = useState(true);
+  const [botCrashedShips, setBotCrashedShips] = useState([]);
+  const [playerCrashedShips, setPlayerCrashedShips] = useState([]);
   const [gameOver, setGameOver] = useState(false);
-  const [playerInjuredShipCoords, setPlayerInjuredShipCoords] = useState([])
-  const [playerShots, setPlayerShots] = useState(0);
-  const [playerExactShots, setPlayerExactShots] = useState(0);
-  const [playerPoints, setPlayerPoints] = useState(0);
-  const [botShots, setBotShots] = useState(0);
-  const [botExactShots, setBotExactShots] = useState(0);
-  const [botPoints, setBotPoints] = useState(0);
+  const [playerInjuredShipCoords, setPlayerInjuredShipCoords] = useState([]);
+  // const [playerShots, setPlayerShots] = useState(0);
+  // const [playerExactShots, setPlayerExactShots] = useState(0);
+  // const [playerPoints, setPlayerPoints] = useState(0);
+  // const [botShots, setBotShots] = useState(0);
+  // const [botExactShots, setBotExactShots] = useState(0);
+  // const [botPoints, setBotPoints] = useState(0);
 
   // Memo Datas
   const playerInfoData = useMemo(() => {
     return {
-      totalShots: playerShots,
-      exactShots: playerExactShots,
-      points: playerPoints,
       username: player.playerName,
       flag: player.flag,
       avatar: player.avatar,
       won: player.won,
       lost: player.lost,
-      sunkenShips: botCrashedShips.length
-    }
-  }, [playerShots, playerExactShots, playerPoints, player, botCrashedShips])
+    };
+  }, [player]);
 
   const botInfoData = useMemo(() => {
     return {
-      totalShots: botShots,
-      exactShots: botExactShots,
-      points: botPoints,
       username: bot.botName,
       flag: bot.flag,
       avatar: bot.avatar,
       won: bot.won,
       lost: bot.lost,
-      sunkenShips: playerCrashedShips.length
-    }
-  }, [botShots, botExactShots, botPoints, bot, playerCrashedShips])
+    };
+  }, [bot]);
 
   // Actions
   const backtoLobby = () => {
@@ -85,132 +95,133 @@ const Game = () => {
     dispatch(deleteBotBoard());
     dispatch(deleteBotShips());
     dispatch(deletePlayerShips());
-  }
+  };
 
   const shotHandler = (x, y, isPlayerBoard, shipId) => {
     if (!isPlayerBoard && playerTurn && !gameOver) {
-      const newBotBoard = JSON.parse(JSON.stringify(botBoard))
-      if (newBotBoard[x][y].shoted) return messageApi.open(ALREADY_SHOTED_MESSAGE)
-      if (sound) shot1()
-      newBotBoard[x][y].shoted = true
-      setPlayerShots(prev => prev + 1)
-      let points = playerPoints + 1;
+      const newBotBoard = JSON.parse(JSON.stringify(botBoard));
+      if (newBotBoard[x][y].shoted)
+        return messageApi.open(ALREADY_SHOTED_MESSAGE);
+      if (sound) shot1();
+      newBotBoard[x][y].shoted = true;
       if (!newBotBoard[x][y].hasShipPart) {
-        setPlayerTurn(false)
+        setPlayerTurn(false);
       }
       if (Boolean(shipId)) {
-        setPlayerExactShots(prev => prev + 1)
-        newBotBoard[x][y].shipStatus = 'injured';
-        points = points + 2
-        const newBotShips = botShips.map(ship => {
+        newBotBoard[x][y].shipStatus = "injured";
+        const newBotShips = botShips.map((ship) => {
           if (shipId === ship.id) {
-            ship.shotedCount++
+            ship.shotedCount++;
             if (ship.shotedCount === ship.length) {
-              if (sound) boom1()
-              ship.crashed = true
-              points = points + (20 - ship.length);
-              setCrashedShipOnBoard(ship, newBotBoard)
-              setBotCrashedShips(prev => [...prev, { ...ship }])
+              if (sound) boom1();
+              ship.crashed = true;
+              setCrashedShipOnBoard(ship, newBotBoard);
+              setBotCrashedShips((prev) => [...prev, { ...ship }]);
             }
           }
-          return ship
-        })
-        setBotShips(JSON.parse(JSON.stringify(newBotShips)))
+          return ship;
+        });
+        setBotShips(JSON.parse(JSON.stringify(newBotShips)));
       }
-      setPlayerPoints(points)
-      setBotBoard(JSON.parse(JSON.stringify(newBotBoard)))
+      setBotBoard(JSON.parse(JSON.stringify(newBotBoard)));
     }
-  }
+  };
 
   // Effects
   useEffect(() => {
-    if (reduxPlayerBoard.length && reduxPlayerShips.length && reduxBotBoard.length && reduxBotShips.length) {
+    if (
+      reduxPlayerBoard.length &&
+      reduxPlayerShips.length &&
+      reduxBotBoard.length &&
+      reduxBotShips.length
+    ) {
       setPlayerBoard(reduxPlayerBoard);
       setPlayerShips(reduxPlayerShips);
       setBotBoard(reduxBotBoard);
       setBotShips(reduxBotShips);
     }
-  }, [reduxPlayerBoard, reduxPlayerShips, reduxBotBoard, reduxBotShips])
+  }, [reduxPlayerBoard, reduxPlayerShips, reduxBotBoard, reduxBotShips]);
 
   useEffect(() => {
     if (playerCrashedShips.length === 10) {
-      dispatch(setBotWinCount())
-      dispatch(setPlayerlostCount())
-      setGameOver(true)
-      messageApi.open(BOT_WIN_MESSAGE)
+      dispatch(setBotWinCount());
+      dispatch(setPlayerlostCount());
+      setGameOver(true);
+      messageApi.open(BOT_WIN_MESSAGE);
     }
-  }, [playerCrashedShips])
+  }, [playerCrashedShips]);
 
   useEffect(() => {
     if (botCrashedShips.length === 10) {
-      dispatch(setPlayerWinCount())
-      dispatch(setBotlostCount())
-      setGameOver(true)
-      messageApi.open(PLAYER_WIN_MESSAGE)
+      dispatch(setPlayerWinCount());
+      dispatch(setBotlostCount());
+      setGameOver(true);
+      messageApi.open(PLAYER_WIN_MESSAGE);
     }
   }, [botCrashedShips]);
 
   useEffect(() => {
     if (!playerTurn && playerBoard && !gameOver) {
-      const shotCoords = getShotCoords(level, playerBoard, playerShips, playerInjuredShipCoords);
-      if (!shotCoords) return console.log('gameover')
+      const shotCoords = getShotCoords(
+        level,
+        playerBoard,
+        playerShips,
+        playerInjuredShipCoords
+      );
+      if (!shotCoords) return console.log("gameover");
       if (Boolean(shotCoords)) {
         const { x, y } = shotCoords;
         if (!playerBoard[x][y].shoted) {
-          let points = botPoints + 1;
           setTimeout(() => {
-            if (sound) shot1()
-            const newPlayerBoard = JSON.parse(JSON.stringify(playerBoard))
+            if (sound) shot1();
+            const newPlayerBoard = JSON.parse(JSON.stringify(playerBoard));
             newPlayerBoard[x][y].shoted = true;
-            setBotShots(prev => prev + 1)
             if (!newPlayerBoard[x][y].hasShipPart) {
-              setPlayerTurn(true)
+              setPlayerTurn(true);
             } else if (Boolean(newPlayerBoard[x][y].shipId)) {
-              setBotExactShots(prev => prev + 1)
-              newPlayerBoard[x][y].shipStatus = 'injured';
-              points = points + 2
-              setPlayerInjuredShipCoords(prev => [...prev, { x, y }])
-              const newPlayerShips = playerShips.map(ship => {
+              newPlayerBoard[x][y].shipStatus = "injured";
+              setPlayerInjuredShipCoords((prev) => [...prev, { x, y }]);
+              const newPlayerShips = playerShips.map((ship) => {
                 if (newPlayerBoard[x][y].shipId === ship.id) {
-                  ship.shotedCount++
+                  ship.shotedCount++;
                   if (ship.shotedCount === ship.length) {
-                    if (sound) boom1()
+                    if (sound) boom1();
                     ship.crashed = true;
-                    points = points + (20 - ship.length);
-                    setPlayerInjuredShipCoords([])
-                    setCrashedShipOnBoard(ship, newPlayerBoard)
-                    setPlayerCrashedShips(prev => [...prev, { ...ship }])
+                    setPlayerInjuredShipCoords([]);
+                    setCrashedShipOnBoard(ship, newPlayerBoard);
+                    setPlayerCrashedShips((prev) => [...prev, { ...ship }]);
                   }
                 }
-                return ship
-              })
-              setPlayerShips(JSON.parse(JSON.stringify(newPlayerShips)))
+                return ship;
+              });
+              setPlayerShips(JSON.parse(JSON.stringify(newPlayerShips)));
             }
-            setBotPoints(points)
-            setPlayerBoard(JSON.parse(JSON.stringify(newPlayerBoard)))
+            setPlayerBoard(JSON.parse(JSON.stringify(newPlayerBoard)));
           }, 1000);
         } else {
-          alert('player turn')
-          setPlayerTurn(true)
+          alert("player turn");
+          setPlayerTurn(true);
         }
       } else {
-        alert('something went wrong');
+        alert("something went wrong");
       }
     }
-  }, [playerTurn, playerBoard])
+  }, [playerTurn, playerBoard]);
 
   // UnMount Effects
   useEffect(() => {
-    return () => backtoLobby()
-  }, [])
+    return () => backtoLobby();
+  }, []);
 
   return (
-    <div className='game_container'>
+    <div className="game_container">
       {contextHolder}
-      <button className='back-button' onClick={backtoLobby}>back</button>
-      <div className='game_container__game'>
-        <div className='game-section player-section'>
-          <div className='pnael-section player-section-panel'>
+      <button className="back-button" onClick={backtoLobby}>
+        back
+      </button>
+      <div className="game_container__game">
+        <div className="game-section player-section">
+          <div className="pnael-section player-section-panel">
             <PlayerInfoPanel infoData={playerInfoData} />
           </div>
           <GameBoard
@@ -221,7 +232,7 @@ const Game = () => {
             gameOver={gameOver}
           />
         </div>
-        <div className='game-section bot-section'>
+        <div className="game-section bot-section">
           <GameBoard
             board={botBoard}
             isPlayerBoard={false}
@@ -229,13 +240,13 @@ const Game = () => {
             playerTurn={playerTurn}
             gameOver={gameOver}
           />
-          <div className='pnael-section bot-section-panel'>
+          <div className="pnael-section bot-section-panel">
             <PlayerInfoPanel infoData={botInfoData} />
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Game
+export default Game;
