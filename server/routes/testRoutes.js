09d2253c7d6a7433288d9799da101b36b2d92b62
arturohanyan1 <arturohanyan100@mail.ru>
2023-07-25 01:04:42 +0400

    fixed api call problem

--- server/routes/testRoutes.js
+++ server/routes/testRoutes.js
@@ -1,28 +1,16 @@
 const router = require("express").Router();
-const { Album } = require('../db/models');
+const { User } = require('../db/models');
 
 router.get("/", async (req, res) => {
-  console.log('server', req, res)
   try {
-    res.json({ dd: 44 });
-    //   const allBlocks = await Block.findAll({ raw: true })
-    //   // console.log(allBlocks);
-    //   res.json(allBlocks);
+    console.log('req.query', req.query)
+    const id = req.query.id
+    const allBlocks = await User.findOne({ where: { id: id } })
+    res.json(allBlocks);
   } catch (err) {
     console.log(err);
     res.sendStatus(500);
   }
-  // router.get('/', async (req, res) => {
-  //   try {
-  //     const user_id = req.session.superuserID;
-  //     // console.log(user_id);
-  //     const albums = await Album.findAll({ where: { user_id }, raw: true });
-  //     // console.log(albums);
-  //     res.json(albums);
-  //   } catch (err) {
-  //     console.log(err);
-  //   }
-  // });
 });
 
 module.exports = router
